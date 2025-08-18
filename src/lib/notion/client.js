d949748d41a58a8212a75fa59e49a4f3388b00f2
Karly Hoffman <karlyhoffman@gmail.com>
2025-08-17 22:40:06 -0500

    notion — fetch database entries

--- src/lib/notion/client.js
+++ src/lib/notion/client.js
@@ -1,4 +1,5 @@
 import { Client } from "@notionhq/client";
+import { RSVP_PROPERTIES } from "./constants";
 
 const auth = import.meta.env.NOTION_TOKEN;
 
@@ -6,11 +7,54 @@ export const notion = new Client({ auth });
 
 export async function getEntries() {
   try {
-    const { results: entries = [] } = await notion.databases.query({
+    const { results = [] } = await notion.databases.query({
       database_id: import.meta.env.NOTION_DATABASE_ID,
     });
 
-    return entries;
+    const entries = results
+      .filter((entry) => "properties" in entry)
+      .reduce((acc, entry) => {
+        const { properties, id } = entry;
+        const data = { id };
+
+        Object.entries(properties).forEach((p) => {
+          const [title, property] = p;
+
+          if (RSVP_PROPERTIES[title]) {
+            /* Title */
+            if (property.type === "title" && property.title[0]?.text?.content) {
+              data[RSVP_PROPERTIES[title].key] = property.title[0].text.content;
+            }
+
+            /* Rich Text */
+            if (
+              property.type === "rich_text" &&
+              property.rich_text[0]?.text?.content
+            ) {
+              data[RSVP_PROPERTIES[title].key] =
+                property.rich_text[0].text.content;
+            }
+
+            /* Select */
+            if (property.type === "select" && property.select?.name) {
+              data[RSVP_PROPERTIES[title].key] = property.select.name;
+            }
+
+            /* Relation */
+            if (property.type === "relation") {
+              data[RSVP_PROPERTIES[title].key] = property.relation.map(
+                (rel) => rel.id
+              );
+            }
+          }
+        });
+
+        acc.push(data);
+
+        return acc;
+      }, []);
+
+    return entries || [];
   } catch (error) {
     console.error(error);
   }
